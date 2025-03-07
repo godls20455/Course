cfaa33578c9184faf052c2db34b126b36abc8f9b
godls20455 <godls20455@ajou.ac.kr>
2025-03-07 16:09:17 +0900

    [Update]  lambda

---
+++ src/lambda.kt
@@ -0,0 +1,32 @@
+fun main() {
+    /* Lambda Expression : 값(value)처럼 다룰 수 있는 익명 함수
+    익명함수 :
+     */
+
+    val sayHello = fun() { println("Hello World!") }
+    sayHello()
+
+    val squareNum: (Int) -> (Int) = { num -> num * num }
+    val squareNum2 = { num: Int -> num * num }
+    val squareNum3: (Int) -> (Int) = { it * it }
+
+    println(squareNum(10))
+    println(squareNum2(10))
+    println(squareNum3(10))
+
+    fun invokeLambda(lambda: (Int) -> Boolean): Boolean {
+
+        return lambda(5)
+    }
+
+    val falseValue = invokeLambda({num -> num == 10})
+    val trueValue = invokeLambda({num -> num < 10})
+    println(falseValue)
+    println(trueValue)
+
+    // 28~31 모두 같은 코드.
+    invokeLambda({num -> num < 10})
+    invokeLambda { it < 10 }
+    invokeLambda (){ it <10 }
+    invokeLambda { it > 10 }
+}
